067bb5a973efd61c2645f8aee5a2d59582e9b00d
Priyanshu Shekhar <priyanshu.shekhar@hotmail.com>
2020-09-30 10:58:36 +0530

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+### Karate Learning
+Trello API tests using Karate.
+
+### How to run:
+`gradle test -Dapp.key=trello_key -Dapp.token=trello_token`
--- src/test/java/com/trello/TrelloRunner.java
+++ src/test/java/com/trello/TrelloRunner.java
@@ -1,21 +1,15 @@
 package com.trello;
 
-import com.intuit.karate.netty.FeatureServer;
-import net.masterthought.cucumber.Reportable;
-import net.masterthought.cucumber.presentation.PresentationMode;
 import org.apache.commons.io.FileUtils;
 import com.intuit.karate.Results;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
